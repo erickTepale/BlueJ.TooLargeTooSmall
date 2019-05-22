0d1bd66a9028ffb6136f6e9270c4c6fbb15942ac
Erick Tepale <e.tepale@gmail.com>
2019-05-22 09:10:06 -0400

    finished too high/low lab

--- Main.java
+++ Main.java
@@ -18,20 +18,22 @@ public class Main
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
         
+        int tempNumber = -1; //stores temp guess
         int mysteryNumber = rand.nextInt(100) + 1;
-        int count = 1;
-        System.out.println(mysteryNumber);
+        int count = 0;
         
-        System.out.println("Can you guess my mystery integer number? It is between 1 - 100");
+        System.out.println("Can you guess my mystery integer number? It is between 1 - 100" + mysteryNumber);
         int guess = input.nextInt();
-        
+        if(tempNumber != guess) count++;
+            
         while(guess != mysteryNumber){
             System.out.println( (guess < mysteryNumber) ? "Too Small Try Again." : "Too Large Try Again." );
-            count++;
+            tempNumber = guess;
             guess = input.nextInt();
+            if(tempNumber != guess) count++;
         }
         
-        
+        if(count == 0) count++;
         System.out.println("You Got It! It only took you " + count + 
                             ( (count > 1) ? " times." : " time.") );
     }
